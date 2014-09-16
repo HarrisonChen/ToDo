15a5328b97334926df67f638c7ac0c190ae2834c
Harrison Chen <harrisonxchen@gmail.com>
2014-09-16 08:42:40 -0700

    remove hardcoded localhost path

--- app/assets/javascripts/angular/models/task.js
+++ app/assets/javascripts/angular/models/task.js
@@ -1,6 +1,6 @@
 angular.module('models.task', [])
 .factory('Task', ['$resource', function($resource){
-  var Task = $resource('http://localhost:3000/api/v1/tasks/:id.json',
+  var Task = $resource('/api/v1/tasks/:id.json',
                        { id: '@id' },
                        { update: { method: 'PUT' } });
 
